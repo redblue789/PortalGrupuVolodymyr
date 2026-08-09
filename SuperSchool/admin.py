6bfcfc628b90f93f4856ccb9d199729311cf9e37
azarvlad9-sudo <azarvlad9@gmail.com>
2026-08-09 13:16:27 +0300

    Add files via upload

--- SuperSchool/admin.py
+++ SuperSchool/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Student, Subject, Grade
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'grade_level', 'description')
+    search_fields = ('last_name', 'first_name', 'grade_level')
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    # Прибрали 'comment', оскільки його немає в моделі Grade
+    list_display = ('student', 'subject', 'score', 'date_added')
+    list_filter = ('subject', 'student__grade_level', 'date_added')
+    search_fields = ('student__last_name', 'student__first_name', 'subject__name')
+    list_editable = ('score',)
--- SuperSchool/views.py
+++ SuperSchool/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student, Grade, Subject
 
-# Create your views here.
+
+def journal_view(request):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+
+        # 1. Додавання учня
+        if action == 'add_student':
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            description = request.POST.get('description', '')
+            if first_name and last_name:
+                Student.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    description=description
+                )
+            return redirect('journal')
+
+        # 2. Додавання оцінки (1-100)
+        elif action == 'add_grade':
+            student_id = request.POST.get('student_id')
+            score = request.POST.get('score')
+            if student_id and score:
+                student = get_object_or_404(Student, id=student_id)
+                subject, _ = Subject.objects.get_or_create(name="Програмування")
+                score_int = int(score)
+                if 1 <= score_int <= 100:
+                    Grade.objects.create(student=student, subject=subject, score=score_int)
+            return redirect('journal')
+
+        # 3. Видалення учня
+        elif action == 'delete_student':
+            student_id = request.POST.get('student_id')
+            if student_id:
+                student = get_object_or_404(Student, id=student_id)
+                student.delete()
+            return redirect('journal')
+
+        # 4. Видалення оцінки
+        elif action == 'delete_grade':
+            grade_id = request.POST.get('grade_id')
+            if grade_id:
+                grade = get_object_or_404(Grade, id=grade_id)
+                grade.delete()
+            return redirect('journal')
+
+    students = Student.objects.prefetch_related('grades').all()
+    return render(request, 'journal.html', {'students': students})
